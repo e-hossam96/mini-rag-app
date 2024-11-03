8b4928d0594f22a8bc063e27e217873c0c31f6bc
e-hossam96 <ehh.hossam@gmail.com>
2024-11-03 22:02:52 +0200

    added two file vars to config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,6 +6,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APPLICATION_NAME: str
     APPLICATION_VERSION: str
+    FILE_ALLOWED_TYPES: list[str]
+    FILE_MAX_SIZE: int
 
     class Config:
         env_file = ".env"
