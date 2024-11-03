9a6860cdc5e8ea05c9eb47ea19f0b04c276929a6
e-hossam96 <ehh.hossam@gmail.com>
2024-11-03 14:51:32 +0200

    used async functions

--- routes/base.py
+++ routes/base.py
@@ -7,7 +7,7 @@ base_router = APIRouter(prefix="/api/v1", tags=["api_v1"])
 
 
 @base_router.get("/")
-def welcome() -> dict:
+async def welcome() -> dict:
     resp = {
         "app_name": os.getenv("APPLICATION_NAME"),
         "app_version": os.getenv("APPLICATION_VERSION"),
