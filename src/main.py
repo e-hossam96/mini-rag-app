a678607d75a50ef58dff4f4f9c4d5db02371ca5b
e-hossam96 <ehh.hossam@gmail.com>
2024-11-03 22:35:46 +0200

    updated mian code to use data router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 """Main FastAPI application."""
 
 from fastapi import FastAPI
-from routes import base
+from routes import base, data
 
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(data.data_router)
