0cef4681716dbeeca517536ddfbbb2465fe4210d
e-hossam96 <ehh.hossam@gmail.com>
2024-11-04 12:52:02 +0200

    added chunk size to enums

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     APPLICATION_VERSION: str
     FILE_ALLOWED_TYPES: list[str]
     FILE_MAX_SIZE: int
+    FILE_CHUNK_SIZE: int
 
     class Config:
         env_file = ".env"
