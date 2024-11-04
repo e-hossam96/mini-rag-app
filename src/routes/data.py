80eb608babfa745135f58b9d1f1c176c750c50dd
e-hossam96 <ehh.hossam@gmail.com>
2024-11-04 11:55:09 +0200

    used response status codes from fastapi

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,8 @@
 """Implementaation of data routers."""
 
 from controllers.DataController import DataController
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 
 data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
@@ -10,8 +11,10 @@ data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
 @data_router.post("/upload/{project_id}")
 async def upload_data(
     project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)
-) -> dict:
+) -> JSONResponse:
     # validate file settings
     is_valid, signal = DataController().validate_upload_file(file)
-    resp = {"status": is_valid, "signal": signal}
+    resp_content = {"status": is_valid, "signal": signal}
+    resp_code = status.HTTP_200_OK if is_valid else status.HTTP_400_BAD_REQUEST
+    resp = JSONResponse(content=resp_content, status_code=resp_code)
     return resp
