1afa275c950cab6a55179011ae284ee08d3e5af9
e-hossam96 <ehh.hossam@gmail.com>
2024-11-04 12:19:18 +0200

    added base_dir path to BaseController class

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,8 +1,11 @@
 """Defining Base Controller class."""
 
+import pathlib
 from helpers.config import get_settings
 
 
 class BaseController:
     def __init__(self) -> None:
         self.app_settings = get_settings()
+        self.base_dir = pathlib.Path(__file__).parent.parent
+        self.files_dir = self.base_dir.joinpath(pathlib.path("assets").joinpath("files"))
