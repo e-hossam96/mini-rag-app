50f3ab3555a22b053602cd9dbce3c81fe3ac6755
e-hossam96 <ehh.hossam@gmail.com>
2024-11-04 12:29:21 +0200

    added path suffix to vars

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -7,5 +7,5 @@ from helpers.config import get_settings
 class BaseController:
     def __init__(self) -> None:
         self.app_settings = get_settings()
-        self.base_dir = pathlib.Path(__file__).parent.parent
-        self.files_dir = self.base_dir.joinpath(pathlib.path("assets").joinpath("files"))
+        self.base_dir_path = pathlib.Path(__file__).parent.parent
+        self.files_dir_path = self.base_dir.joinpath(pathlib.path("assets").joinpath("files"))
