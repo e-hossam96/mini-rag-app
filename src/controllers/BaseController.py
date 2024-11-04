1172edf8f7e835050e6cb5beedc158730b9d2da9
e-hossam96 <ehh.hossam@gmail.com>
2024-11-04 13:19:44 +0200

    fixed error in var names

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -8,4 +8,4 @@ class BaseController:
     def __init__(self) -> None:
         self.app_settings = get_settings()
         self.base_dir_path = pathlib.Path(__file__).parent.parent
-        self.files_dir_path = self.base_dir.joinpath(pathlib.path("assets").joinpath("files"))
+        self.files_dir_path = self.base_dir_path.joinpath(pathlib.Path("assets").joinpath("files"))
