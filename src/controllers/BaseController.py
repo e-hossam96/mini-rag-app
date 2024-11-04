0d8132f0268a811f3cf849d508e2247aab9226cd
e-hossam96 <ehh.hossam@gmail.com>
2024-11-04 13:56:59 +0200

    added function to generate random string

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,5 +1,7 @@
 """Defining Base Controller class."""
 
+import string
+import random
 import pathlib
 from helpers.config import get_settings
 
@@ -7,6 +9,12 @@ from helpers.config import get_settings
 class BaseController:
     def __init__(self) -> None:
         self.app_settings = get_settings()
-        self.file_scale = 1_048_576 # from MiBs to Bytes
+        self.file_scale = 1_048_576  # from MiBs to Bytes
         self.base_dir_path = pathlib.Path(__file__).parent.parent
-        self.files_dir_path = self.base_dir_path.joinpath(pathlib.Path("assets").joinpath("files"))
+        self.files_dir_path = self.base_dir_path.joinpath(
+            pathlib.Path("assets").joinpath("files")
+        )
+
+    def generate_random_string(self, length: int = 12) -> str:
+        population = string.ascii_lowercase + string.digits
+        return "".join(random.choices(population=population, k=length))
