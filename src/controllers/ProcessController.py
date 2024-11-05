b30a33981db23bf27ad7e8800be69b91188b3fc1
e-hossam96 <ehh.hossam@gmail.com>
2024-11-05 19:50:43 +0200

    added method to get file content

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -5,6 +5,7 @@ from typing import Union
 from .BaseController import BaseController
 from .ProjectController import ProjectController
 from models.enums.ProcessConfig import ProcessConfig
+from langchain_core.documents.base import Document
 from langchain_community.document_loaders import TextLoader, PyMuPDFLoader
 
 
@@ -28,3 +29,8 @@ class ProcessController(BaseController):
         elif file_ext == ProcessConfig.PDF.value:
             file_loader = PyMuPDFLoader(str(file_path))
         return file_loader
+
+    def get_file_content(self, process_file_id: str) -> list[Document]:
+        file_loader = self.get_file_loader(process_file_id)
+        file_content = file_loader.load()
+        return file_content
