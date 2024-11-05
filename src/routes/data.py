b986d155688501ae072172977334d7dbb5611b34
e-hossam96 <ehh.hossam@gmail.com>
2024-11-05 18:12:45 +0200

    added data scheme to process fn signature

--- src/routes/data.py
+++ src/routes/data.py
@@ -4,6 +4,7 @@ from controllers.DataController import DataController
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
+from .schemes.data import ProcessRequest
 
 data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
 
@@ -35,3 +36,8 @@ async def upload_data(
             content=resp_content, status_code=status.HTTP_400_BAD_REQUEST
         )
     return resp
+
+
+@data_router.post("/process/{project_id}")
+async def process_data(project_id: str, process_request: ProcessRequest) -> str:
+    pass
