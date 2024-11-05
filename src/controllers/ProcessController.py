08465643ebdcf7c5426360ea12e16ec288d65a7c
e-hossam96 <ehh.hossam@gmail.com>
2024-11-05 18:22:02 +0200

    defined method to get file extention

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -1,7 +1,15 @@
 """Implementing data processing controller class."""
 
+import pathlib
 from .BaseController import BaseController
+from .ProjectController import ProjectController
 
 
 class ProcessController(BaseController):
-    pass
+    def __init__(self, project_id: str) -> None:
+        super().__init__()
+        self.project_id = project_id
+        self.project_path = ProjectController().get_project_path(self.project_id)
+
+    def get_file_ext(process_file_id: str) -> str:
+        return pathlib.Path(process_file_id).suffix
