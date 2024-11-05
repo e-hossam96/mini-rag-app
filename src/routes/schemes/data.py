75bffcd8d8c2ec9db5831965aaf0e6ebacd3e621
e-hossam96 <ehh.hossam@gmail.com>
2024-11-05 17:07:14 +0200

    added process request variables

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -1,7 +1,11 @@
 """Implementing data processing request schema."""
 
+from typing import Optional
 from pydantic import BaseModel
 
 
 class ProcessRequest(BaseModel):
-    pass
+    process_file_id: str
+    process_chunk_size: Optional[int] = 100
+    process_overlap_size: Optional[int] = 20
+    process_do_reset: Optional[bool] = False
