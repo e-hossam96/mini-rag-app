574dd916800e3bcbe92b673bfeb87117e7239865
e-hossam96 <ehh.hossam@gmail.com>
2024-11-06 22:48:00 +0200

    implemented project database schema

--- src/models/db_schemes/project.py
+++ src/models/db_schemes/project.py
@@ -1,7 +1,20 @@
 """Implementing project schema for mongo database."""
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from bson.objectid import ObjectId
+from typing import Optional
 
 
 class Project(BaseModel):
-    pass
+    _id: Optional[ObjectId]
+    project_id: str = Field(min_length=1)
+
+    @field_validator("project_id")
+    @classmethod
+    def validate_project_id(cls, value: str) -> str:
+        if not value.isalnum():
+            raise ValueError("{project_id} must be Alpha Numeric")
+        return value
+
+    class config:
+        arbitrary_types_allowed = True
