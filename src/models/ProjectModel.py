798190c6b4f4cad6c09ba25d81a9bb781a88c4af
e-hossam96 <ehh.hossam@gmail.com>
2024-11-07 16:46:02 +0200

    fixed type hint error for database client

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -1,9 +1,9 @@
 """Defining Project Model class and its methods."""
 
 from .BaseDataModel import BaseDataModel
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
 
 class ProjectModel(BaseDataModel):
-    def __init__(self, db_client: AsyncIOMotorClient) -> None:
+    def __init__(self, db_client: AsyncIOMotorDatabase) -> None:
         super().__init__(db_client)
