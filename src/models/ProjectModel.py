5d1fb919e50d95e49ba9525cc20e8e2d1737fe9b
e-hossam96 <ehh.hossam@gmail.com>
2024-11-07 16:56:46 +0200

    added method to create project in db from project pydantic model

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -3,6 +3,7 @@
 from .BaseDataModel import BaseDataModel
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from .enums.DatabaseConfig import DatabaseConfig
+from db_schemes.project import Project
 
 
 class ProjectModel(BaseDataModel):
@@ -11,3 +12,8 @@ class ProjectModel(BaseDataModel):
         self.db_collection = self.db_client[
             DatabaseConfig.PROJECT_COLLECTION_NAME.value
         ]
+
+    async def create_project(self, project: Project) -> Project:
+        project_db_id = await self.db_collection.insert_one(**project.model_dump_json())
+        project._id = project_db_id.inserted_id
+        return project
