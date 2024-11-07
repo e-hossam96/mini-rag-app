615d1e43cf77d54d3867b518981204160f747bcb
e-hossam96 <ehh.hossam@gmail.com>
2024-11-07 19:32:36 +0200

    added method to return chunk give chunk_id

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -4,6 +4,8 @@ from .BaseDataModel import BaseDataModel
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from .enums.DatabaseConfig import DatabaseConfig
 from .db_schemes.data_chunk import DataChunk
+from typing import Union
+from bson.objectid import ObjectId
 
 
 class ChunkModel(BaseDataModel):
@@ -15,3 +17,9 @@ class ChunkModel(BaseDataModel):
         chunk_db_id = await self.db_collection.insert_one(chunk.model_dump())
         chunk._id = chunk_db_id.inserted_id
         return chunk
+
+    async def get_chunk(self, chunk_id: str) -> Union[DataChunk, None]:
+        record = await self.db_collection.find_one({"_id": ObjectId(chunk_id)})
+        if record is None:
+            return None
+        return DataChunk(**record)
