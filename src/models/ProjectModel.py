0871eae4338762ee429d940719964c74af6f4359
e-hossam96 <ehh.hossam@gmail.com>
2024-11-07 16:48:38 +0200

    finished ProjectModel constructor

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -2,8 +2,12 @@
 
 from .BaseDataModel import BaseDataModel
 from motor.motor_asyncio import AsyncIOMotorDatabase
+from .enums.DatabaseConfig import DatabaseConfig
 
 
 class ProjectModel(BaseDataModel):
     def __init__(self, db_client: AsyncIOMotorDatabase) -> None:
         super().__init__(db_client)
+        self.db_collection = self.db_client[
+            DatabaseConfig.PROJECT_COLLECTION_NAME.value
+        ]
