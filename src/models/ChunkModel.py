38a5a4ce5c1ca38183ac7a87d5835e1a43bbd401
e-hossam96 <ehh.hossam@gmail.com>
2024-11-07 19:25:13 +0200

    added method to create data chunk

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -10,3 +10,8 @@ class ChunkModel(BaseDataModel):
     def __init__(self, db_client: AsyncIOMotorDatabase) -> None:
         super().__init__(db_client)
         self.db_collection = self.db_client[DatabaseConfig.CHUNK_COLLECTION_NAME.value]
+
+    async def create_chunk(self, chunk: DataChunk) -> DataChunk:
+        chunk_db_id = await self.db_collection.insert_one(chunk.model_dump())
+        chunk._id = chunk_db_id.inserted_id
+        return chunk
