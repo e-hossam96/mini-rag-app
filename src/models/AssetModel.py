1756344f0bc6a5934ea5e8895c0efd6d086cf042
e-hossam96 <ehh.hossam@gmail.com>
2024-11-09 00:50:53 +0200

    implemented AssetModel and all its methods

---
+++ src/models/AssetModel.py
@@ -0,0 +1,40 @@
+"""Defining Asset Model class and its methods."""
+
+from .BaseDataModel import BaseDataModel
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from .enums.DatabaseConfig import DatabaseConfig
+from .db_schemes.asset import Asset
+from bson.objectid import ObjectId
+
+
+class AssetModel(BaseDataModel):
+    def __init__(self, db_client: AsyncIOMotorDatabase) -> None:
+        super().__init__(db_client)
+        self.collection_name = DatabaseConfig.ASSET_COLLECTION_NAME.value
+        self.db_collection = self.db_client[self.collection_name]
+
+    async def init_collection(self) -> None:
+        collection_names = await self.db_client.list_collection_names()
+        if self.collection_name not in collection_names:
+            indexes = Asset.get_indexes()
+            for index in indexes:
+                await self.db_collection.create_index(**index)
+
+    @classmethod
+    async def create_instance(
+        cls, db_client: AsyncIOMotorDatabase
+    ) -> AsyncIOMotorDatabase:
+        instance = cls(db_client)
+        await instance.init_collection()
+        return instance
+
+    async def create_asset(self, asset: Asset) -> Asset:
+        asset_db_id = await self.db_collection.insert_one(asset.model_dump())
+        asset._id = asset_db_id.inserted_id
+        return asset
+
+    async def get_all_project_assets(self, project_id: str) -> list[dict]:
+        records = await self.db_collection.find(
+            {"asset_project_id": ObjectId(project_id)}
+        ).to_list()
+        return records
