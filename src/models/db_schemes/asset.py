d529c42e1b94fea40cf1a15d0ae5ab523faa1a19
e-hossam96 <ehh.hossam@gmail.com>
2024-11-09 00:23:36 +0200

    defined asset pydantic model and its get_indexes static method

---
+++ src/models/db_schemes/asset.py
@@ -0,0 +1,37 @@
+"""Implementing asset schema for mongo database."""
+
+from pydantic import BaseModel, Field
+from bson.objectid import ObjectId
+from typing import Optional
+import datetime
+
+
+class Asset(BaseModel):
+    _id: Optional[ObjectId]
+    asset_project_id: ObjectId
+    asset_name: str = Field(min_length=1)
+    asset_type: str = Field(min_length=1)
+    asset_size: int = Field(ge=0, default=None)  # size in bytes
+    asset_pushed_at: datetime.datetime = Field(
+        default=datetime.datetime.now(datetime.timezone.utc)
+    )
+    asset_config: dict
+
+    @classmethod
+    def get_indexes(cls) -> list[dict]:
+        indexes = [
+            {
+                "keys": [("asset_project_id", 1)],
+                "name": "asset_project_id_index_1",
+                "unique": False,
+            },
+            {
+                "keys": [("asset_project_id", 1), ("asset_name", 1)],
+                "name": "asset_project_id_name_index_1",
+                "unique": True,
+            },
+        ]
+        return indexes
+
+    class Config:
+        arbitrary_types_allowed = True
