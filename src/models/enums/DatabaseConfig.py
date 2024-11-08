6db8328d06b8fe574e773347834f41c165ccd640
e-hossam96 <ehh.hossam@gmail.com>
2024-11-09 00:27:24 +0200

    added asset collection name

--- src/models/enums/DatabaseConfig.py
+++ src/models/enums/DatabaseConfig.py
@@ -6,3 +6,4 @@ from enum import Enum
 class DatabaseConfig(Enum):
     PROJECT_COLLECTION_NAME: str = "projects"
     CHUNK_COLLECTION_NAME: str = "chunks"
+    ASSET_COLLECTION_NAME: str = "assets"
