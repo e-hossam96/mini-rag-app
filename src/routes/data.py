d7e66c5863d3f5a0814c32008d62867eff1b951b
e-hossam96 <ehh.hossam@gmail.com>
2024-11-09 15:41:42 +0200

    added codes to process all project file assets when file_id is not given

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,5 +1,6 @@
 """Implementaation of data routers."""
 
+import logging
 from controllers.DataController import DataController
 from controllers.ProcessController import ProcessController
 from fastapi import APIRouter, UploadFile, status, Request
@@ -70,37 +71,69 @@ async def process_data(
     process_overlap_size = process_request.process_overlap_size
     process_do_reset = process_request.process_do_reset
     process_controller = ProcessController(project_id)
-    file_content = process_controller.get_file_content(process_file_id)
-    if file_content is None:
-        return JSONResponse(
-            content={"signal": ResponseSignal.FILE_PROCESS_FAILED.value},
-            status_code=status.HTTP_400_BAD_REQUEST,
-        )
-    chunks = process_controller.process_file_content(
-        file_content, process_chunk_size, process_overlap_size
-    )
-    # get project _id from db
+
+    # define all needed models
     project_model = await ProjectModel.create_instance(request.app.db_client)
     project = await project_model.get_project(project_id=project_id)
-    # create data chunk objects
-    data_chunks = [
-        DataChunk(
-            chunk_text=chunk.page_content,
-            chunk_metadata=chunk.metadata,
-            chunk_index=i,
-            chunk_project_id=project._id,
-        )
-        for i, chunk in enumerate(chunks)
-    ]
-    # write chunks to db using chunk model object
     chunk_model = await ChunkModel.create_instance(request.app.db_client)
+    asset_model = await AssetModel.create_instance(request.app.db_client)
+
+    # get file ids/names
+    process_file_ids = []
+    if process_file_id is None:
+        # get all project asset file ids/names (files only now)
+        project_assets = await asset_model.get_all_project_assets(
+            project._id, AssetConfig.FILE_TYPE_NAME.value
+        )
+        asset_ids = [asset.asset_name for asset in project_assets]
+        process_file_ids.extend(asset_ids)
+    else:
+        process_file_ids.append(process_file_id)
+
+    if not process_file_ids:
+        return JSONResponse(
+            content={"signal": ResponseSignal.NO_FILES_ERROR.value},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
     if process_do_reset:
         _ = await chunk_model.clear_project_chunks(project._id)  # id in db
-    num_inserted_chunks = await chunk_model.batch_insert_chunks(data_chunks)
+
+    num_chunks = 0
+    num_files = 0
+    # loop over all file ids/names and process them
+    for process_file_id in process_file_ids:
+        logging.info(f"Processing file: {process_file_id}")
+        file_content = process_controller.get_file_content(process_file_id)
+        if file_content is None:
+            # return JSONResponse(
+            #     content={"signal": ResponseSignal.FILE_PROCESS_FAILED.value},
+            #     status_code=status.HTTP_400_BAD_REQUEST,
+            # )
+            logging.error(f"File missing or empty content for file: {process_file_id}")
+            continue
+        chunks = process_controller.process_file_content(
+            file_content, process_chunk_size, process_overlap_size
+        )
+        # create data chunk objects
+        data_chunks = [
+            DataChunk(
+                chunk_text=chunk.page_content,
+                chunk_metadata=chunk.metadata,
+                chunk_index=i,
+                chunk_project_id=project._id,
+            )
+            for i, chunk in enumerate(chunks)
+        ]
+        # write chunks to db using chunk model object
+        num_chunks += await chunk_model.batch_insert_chunks(data_chunks)
+        num_files += 1
+
     return JSONResponse(
         content={
             "signal": ResponseSignal.FILE_PROCESS_SUCCEEDED.value,
-            "num_chunks": num_inserted_chunks,
+            "num_chunks": num_chunks,
+            "num_files": num_files,
         },
         status_code=status.HTTP_200_OK,
     )
