3b02e622a34f7af8f4d269a498f231bebd673d13
e-hossam96 <ehh.hossam@gmail.com>
2024-11-09 18:27:53 +0200

    added signel file error signal

--- src/models/enums/ResponseSignal.py
+++ src/models/enums/ResponseSignal.py
@@ -12,3 +12,4 @@ class ResponseSignal(Enum):
     FILE_PROCESS_SUCCEEDED: str = "File_Processing_Succeeded"
     FILE_PROCESS_FAILED: str = "File_Processing_Failed"
     NO_FILES_ERROR: str = "No_Files_Found_in_Database"
+    NO_FILE_ERROR: str = "File_Not_Found_in_Database"
