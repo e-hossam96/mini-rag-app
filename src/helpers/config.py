121e3673f7a2cfdfd92c61f7a55307530170d24a
e-hossam96 <ehh.hossam@gmail.com>
2024-11-10 22:14:30 +0200

    added LLM settings to pydantic settings config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,11 +6,31 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APPLICATION_NAME: str
     APPLICATION_VERSION: str
+
     FILE_ALLOWED_TYPES: list[str]
     FILE_MAX_SIZE: int
     FILE_CHUNK_SIZE: int
+
     MONGODB_URL: str
     MONGODB_DATABASE: str
+
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_BASE_URL: str = None
+
+    COHERE_API_KEY: str = None
+    COHERE_BASE_URL: str = None
+
+    GENERATION_MODEL_ID: str = None
+
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+
+    MAX_INPUT_CHARACTERS: int = None
+    MAX_OUTPUT_TOKENS: int = None
+    TEMPERATURE: float = None
 
     class Config:
         env_file = ".env"
