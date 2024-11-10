89b57b935f5eed8a51f90311e9df85c00785d9f2
e-hossam96 <ehh.hossam@gmail.com>
2024-11-10 20:55:02 +0200

    initialized cohere provider class

---
+++ src/stores/llm/providers/CoHereProvider.py
@@ -0,0 +1,12 @@
+"""Impolementing CoHere Provider class."""
+
+import logging
+import cohere
+from typing import Union
+from ..LLMConfig import LLMConfig, CoHereConfig
+from ..LLMInterface import LLMInterface
+
+
+class CoHereProvider(LLMInterface):
+    def __init__(self) -> None:
+        pass
