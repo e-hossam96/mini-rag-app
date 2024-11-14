866c639d95c4f2f4b09f51d1f1887243b9621d8c
e-hossam96 <ehh.hossam@gmail.com>
2024-11-14 21:38:00 +0200

    finished implementing constructor

--- src/stores/vectordb/providers/QdrandDBProvider.py
+++ src/stores/vectordb/providers/QdrandDBProvider.py
@@ -1,7 +1,20 @@
 """Implementing Qdrant Vector DB Provider class"""
 
+import pathlib
+import logging
+from qdrant_client import models
+from ..VectorDBConfig import DistanceMethodConfig
 from ..VectorDBInterface import VectorDBInterface
 
 
 class QdrantDBProvider(VectorDBInterface):
-    pass
+    def __init__(self, db_path: pathlib.Path, distance_method: str) -> None:
+        self.client = None
+        self.db_path = db_path
+        # define distance metric
+        # only support cosine and dot
+        if distance_method == DistanceMethodConfig.COSINE.value:
+            self.distance_method = models.Distance.COSINE
+        elif distance_method == DistanceMethodConfig.DOT.value:
+            self.distance_method = models.Distance.DOT
+        self.logger = logging.getLogger(__file__)
