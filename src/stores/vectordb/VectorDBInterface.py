596b465811588db1b5b47733363a224af06a65ca
e-hossam96 <ehh.hossam@gmail.com>
2024-11-14 17:12:21 +0200

    implemented initial version of vector db provider interface

---
+++ src/stores/vectordb/VectorDBInterface.py
@@ -0,0 +1,63 @@
+"""Implementing the Vector DB Interface class."""
+
+from typing import Union, Optional
+from abc import ABC, abstractmethod
+
+
+class VectorDBInterface(ABC):
+    @abstractmethod
+    def connect(self) -> bool:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> bool:
+        pass
+
+    @abstractmethod
+    def is_collection(self, collection_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def list_all_collections(self) -> list:
+        pass
+
+    @abstractmethod
+    def get_collection_info(self, collection_name: str) -> Union[dict, None]:
+        pass
+
+    @abstractmethod
+    def delete_collection(self, collection_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def create_collection(
+        self, collection_name: str, embedding_size: int, do_reset: bool = False
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def insert_one(
+        self,
+        collection_name: str,
+        vector: list[float],
+        metadata: dict,
+        record_id: Optional[str] = None,
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def inset_many(
+        self,
+        collection_name: str,
+        vectors: list[list[float]],
+        metadata: list[dict],
+        record_ids: Optional[list[str]] = None,
+        batch_size: int = 64,
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def search_by_vector(
+        self, collection_name: str, vector: list[float], limit: int = 10
+    ):
+        pass
