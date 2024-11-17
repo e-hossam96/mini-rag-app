50536e836b22084027042a013f60c9478bbb8635
e-hossam96 <ehh.hossam@gmail.com>
2024-11-17 22:37:31 +0200

    added method to get collection name as str

--- src/controllers/VectorDBController.py
+++ src/controllers/VectorDBController.py
@@ -4,6 +4,9 @@ import pathlib
 from .BaseController import BaseController
 from fastapi import FastAPI
 from helpers.config import Settings
+from models.db_schemes.VectorDBDoc import VectorDBDoc
+from models.ChunkDataModel import ChunkDataModel
+from models.ProjectDataModel import ProjectDataModel
 
 
 class VectorDBController(BaseController):
@@ -20,3 +23,6 @@ class VectorDBController(BaseController):
         # in case project has nested directories
         vectordb_path.mkdir(parents=True, exist_ok=True)
         return vectordb_path
+
+    def get_collection_name(self, project_id: str) -> str:
+        return f"collection_{project_id}".strip()
